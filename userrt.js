42120ffaafe4d0b6eafdd1f66c32185568be56c6
kanipakamPavaniTeja-bugTracker <155219657+kanipakamPavaniTeja-bugTracker@users.noreply.github.com>
2025-02-01 17:15:37 +0530

    Delete userrt.js

--- userrt.js
+++
@@ -1,7 +0,0 @@
-let express=require("express")
-const { getdata, search, add } = require("../controller/userc")
-let route=new express.Router()
-route.get("/data",getdata)
-route.get("/search/:id",search)
-route.post("/add",add)
-module.exports=route
